e4a2ecaf24b40afe90b06b507696ca380a95db98
smlu <smlu@s5.net>
2018-11-11 00:13:10 +0100

    Fix  'iequal' multi definitions

--- src/libim/utils/utils.h
+++ src/libim/utils/utils.h
@@ -6,7 +6,7 @@
 
 namespace libim::utils {
     namespace detail {
-        bool compareChar(char c1, char c2)
+        inline bool compareChar(char c1, char c2)
         {
             if(c1 == c2) {
                 return true;
@@ -18,14 +18,15 @@ namespace libim::utils {
         }
     }
 
-    bool iequal(const std::string& s1, const std::string& s2)
+    /* Case insensitive comparison of two strings */
+    inline bool iequal(const std::string& s1, const std::string& s2)
     {
         return ((s1.size() == s2.size()) &&
             std::equal(s1.begin(), s1.end(), s2.begin(), &detail::compareChar)
         );
     }
 
-    bool iequal(std::string_view s1, std::string_view s2)
+    inline bool iequal(std::string_view s1, std::string_view s2)
     {
         return ((s1.size() == s2.size()) &&
             std::equal(s1.begin(), s1.end(), s2.begin(), &detail::compareChar)

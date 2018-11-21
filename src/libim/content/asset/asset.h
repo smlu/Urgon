542211dafdd619f5335fd39ba1cba9fd3ae52a03
smlu <smlu@s5.net>
2018-11-21 23:52:57 +0100

    Add abstract class Asset

---
+++ src/libim/content/asset/asset.h
@@ -0,0 +1,11 @@
+#ifndef LIBIM_ASSET_H
+#define LIBIM_ASSET_H
+
+namespace libim::asset {
+    class Asset
+    {
+    public:
+        virtual ~Asset() = default;
+    };
+}
+#endif // LIBIM_ASSET_H

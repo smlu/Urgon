32bc87432cb2180033425d9cc78bd97e72d232fc
smlu <smlu@s5.net>
2019-01-28 23:20:34 +0100

    Add class 'Sound'

---
+++ src/libim/content/audio/impl/sound.cpp
@@ -0,0 +1,118 @@
+#include "../sound.h"
+#include "wav.h"
+#include "indywv.h"
+#include "../../../io/stream.h"
+#include <exception>
+
+using namespace libim;
+using namespace libim::content;
+using namespace libim::content::audio;
+
+Sound::Sound(std::weak_ptr<ByteArray> wptrBankData, std::size_t dirNameOffset, std::size_t nameOffset, std::size_t dataOffset, std::size_t dataSize) :
+    dirNameOffset_(dirNameOffset),
+    nameOffset_(nameOffset),
+    dataOffset_(dataOffset),
+    dataSize_(dataSize),
+    wptrData_(wptrBankData)
+{}
+
+std::string_view Sound::name() const
+{
+    auto ptrData = lock_or_throw();
+    const char* pName = reinterpret_cast<char*>(&ptrData->at(nameOffset_));
+    return std::string_view(pName);
+}
+
+bool Sound::isValid() const
+{
+    auto ptrData = wptrData_.lock();
+    return ptrData && is_valid(*ptrData);
+
+}
+
+std::shared_ptr<ByteArray> Sound::lock_or_throw() const
+{
+    auto ptrData = wptrData_.lock();
+    if(!ptrData) {
+        std::logic_error("Dead sound object");
+    }
+    return ptrData;
+}
+
+bool Sound::is_valid(const ByteArray& data) const
+{
+    return dataOffset_ + dataSize_  < data.size() &&
+            dirNameOffset_ < data.size() &&
+            dirNameOffset_ <= nameOffset_ &&
+            nameOffset_ < data.size() &&
+            sampleRate_ > 0 &&
+            bitsPerSample_ > 0 &&
+            numChannels_ > 0;
+}
+
+//Sound& Sound::deserialize(const InputStream& istream)
+//{
+//    return *this;
+//}
+
+//Sound& Sound::deserialize(const InputStream&& istream)
+//{
+
+//    return *this;
+//}
+
+bool Sound::serialize(OutputStream&& ostream) const
+{
+    return serialize(ostream);
+}
+
+bool Sound::serialize(OutputStream& ostream) const
+{
+
+
+    WavHeader wavHeader;
+    wavHeader.fmt.size          = 16;
+    wavHeader.fmt.audioFormat   = AudioFormat::LPCM;
+    wavHeader.fmt.numChannels   = numChannels_; // TODO: safe cast
+    wavHeader.fmt.sampleRate    = sampleRate_; // TODO: safe cast
+    wavHeader.fmt.blockAlign    = numChannels_ * bitsPerSample_/ 8; // TODO: safe cast
+    wavHeader.fmt.byteRate      = sampleRate_ * wavHeader.fmt.blockAlign; // TODO: safe cast
+    wavHeader.fmt.bitsPerSample = bitsPerSample_; // TODO: safe cast
+
+    WavDataChunk dataChunk;
+    dataChunk.data = data();
+    if(dataChunk.data.empty() && dataSize_ > 0) {
+        std::logic_error("Cannot serialize invalid sound object!");
+    }
+
+    dataChunk.size = dataChunk.data.size(); // TODO: safe cast
+    wavHeader.size = 36 + dataChunk.size;
+
+    ostream << wavHeader
+            << dataChunk;
+
+    return true;
+}
+
+ByteArray Sound::data() const
+{
+    ByteArray bytes;
+    auto ptrData = lock_or_throw();
+    if(!is_valid(*ptrData)) {
+        return bytes;
+    }
+
+    auto itBegin = ptrData->begin() + dataOffset_; //TODO: safe cast
+    auto itEnd = itBegin + dataSize_; //TODO: safe cast
+    if(isIndyWVFormat_) {
+        bytes= IndyVW::Inflate(InputBinaryStream(*ptrData, itBegin, itEnd));
+    }
+    else
+    {
+        bytes.reserve(dataSize_);
+        std::copy(itBegin, itEnd, std::back_inserter(bytes));
+    }
+
+    return bytes;
+}
+
---
+++ src/libim/content/audio/sound.h
@@ -0,0 +1,112 @@
+#ifndef LIBIM_SOUND_H
+#define LIBIM_SOUND_H
+#include "../../common.h"
+#include "../../io/stream.h"
+
+#include <memory>
+#include <string_view>
+
+namespace libim::content::audio {
+    class Sound
+    {
+    public:
+        Sound() = default;
+        Sound(std::weak_ptr<ByteArray> wptrBankData, std::size_t dirNameOffset, std::size_t nameOffset, std::size_t dataOffset, std::size_t dataSize);
+        Sound(Sound&&) noexcept = default;
+        Sound& operator =(Sound&&) noexcept = default;
+        Sound(const Sound&) = default;
+        Sound& operator =(const Sound&) = default;
+
+
+        std::string_view name() const;
+
+        uint32_t setId(uint32_t id)
+        {
+            return id_ = id;
+        }
+
+        uint32_t id() const
+        {
+            return id_;
+        }
+
+        void setIdx(uint32_t idx)
+        {
+            idx_ = idx;
+        }
+
+        uint32_t idx() const
+        {
+            return idx_;
+        }
+
+        void setSampleRate(std::size_t rate)
+        {
+            sampleRate_ = rate;
+        }
+
+        std::size_t sampleRate() const
+        {
+            return sampleRate_;
+        }
+
+        void setBitsPerSample(std::size_t bps)
+        {
+            bitsPerSample_ = bps;
+        }
+
+        std::size_t bitsPerSample() const
+        {
+            return bitsPerSample_;
+        }
+
+        void setChannels(std::size_t num)
+        {
+            numChannels_ = num;
+        }
+
+        std::size_t channels() const
+        {
+            return numChannels_;
+        }
+
+        void setIndyWVFormat(bool isiwvFormat)
+        {
+            isIndyWVFormat_ = isiwvFormat;
+        }
+
+        bool isIndyWVFormat() const
+        {
+            return isIndyWVFormat_;
+        }
+
+        bool isValid() const;
+
+        //Sound& deserialize(const InputStream& istream);
+        //Sound& deserialize(const InputStream&& istream);
+        bool serialize(OutputStream&& ostream) const;
+        bool serialize(OutputStream& ostream) const;
+
+    private:
+        std::shared_ptr<ByteArray> lock_or_throw() const;
+        bool is_valid(const ByteArray& data) const;
+        ByteArray data() const;
+
+    private:
+        uint32_t id_  = 0;
+        uint32_t idx_ = 0;
+        std::size_t dirNameOffset_;
+        std::size_t nameOffset_;
+        std::size_t dataOffset_;
+        std::size_t dataSize_;
+        std::size_t sampleRate_;
+        std::size_t bitsPerSample_;
+        std::size_t numChannels_;
+        bool isIndyWVFormat_;
+
+        std::weak_ptr<ByteArray> wptrData_;
+    };
+}
+
+
+#endif // LIBIM_SOUND_H

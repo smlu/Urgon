42a873d705fcaf3dec3847451c3894a03a0199b5
smlu <smlu@s5.net>
2019-05-18 23:58:08 +0200

    Update constant 'kCndMatNameLen' to be equal to the constant 'kCndMaxNameLen'

--- src/libim/content/asset/world/impl/serialization/binary/material/cnd_mat_header.h
+++ src/libim/content/asset/world/impl/serialization/binary/material/cnd_mat_header.h
@@ -1,9 +1,10 @@
 #ifndef LIBIM_CND_MAT_HEADER_H
 #define LIBIM_CND_MAT_HEADER_H
+#include "../cnd.h"
 #include "../../../../../material/colorformat.h"
 #include <cstddef>
 namespace libim::content::asset {
-    constexpr static std::size_t kCndMatNameLen = 64;
+    constexpr static std::size_t kCndMatNameLen = kCndMaxNameLen;
     struct CndMatHeader final
     {
         char name[kCndMatNameLen];

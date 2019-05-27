aaf0beef9f3a50ef2b2e6d5df64a6496dc2c9b36
smlu <smlu@s5.net>
2019-05-27 22:30:12 +0200

    Add constant 'kResName_Copyright'

--- src/libim/content/text/impl/text_resource_literals.h
+++ src/libim/content/text/impl/text_resource_literals.h
@@ -7,20 +7,21 @@ namespace libim::content::text {
     using namespace std::string_view_literals;
 
     static constexpr auto kResLabelPunc     = ":"sv;
-    static constexpr auto kResSectionHeader = "###############"sv;
-    static constexpr auto kResName_Entries  = "ENTRIES"sv;
-    static constexpr auto kResName_Flags    = "FLAGS"sv;
-    static constexpr auto kResName_Frames   = "FRAMES"sv;
-    static constexpr auto kResName_Fps      = "FPS"sv;
-    static constexpr auto kResName_Header   = "HEADER"sv;
-    static constexpr auto kResName_Joints   = "JOINTS"sv;
-    static constexpr auto kResName_KfNodes  = "KEYFRAME NODES"sv;
-    static constexpr auto kResName_Markers  = "MARKERS"sv;
-    static constexpr auto kResName_MeshName = "MESH NAME"sv;
-    static constexpr auto kResName_Node     = "NODE"sv;
-    static constexpr auto kResName_Nodes    = "NODES"sv;
-    static constexpr auto kResName_Section  = "SECTION"sv;
-    static constexpr auto kResName_Type     = "TYPE"sv;
+    static constexpr auto kResSectionHeader  = "###############"sv;
+    static constexpr auto kResName_Copyright = "COPYRIGHT"sv;
+    static constexpr auto kResName_Entries   = "ENTRIES"sv;
+    static constexpr auto kResName_Flags     = "FLAGS"sv;
+    static constexpr auto kResName_Frames    = "FRAMES"sv;
+    static constexpr auto kResName_Fps       = "FPS"sv;
+    static constexpr auto kResName_Header    = "HEADER"sv;
+    static constexpr auto kResName_Joints    = "JOINTS"sv;
+    static constexpr auto kResName_KfNodes   = "KEYFRAME NODES"sv;
+    static constexpr auto kResName_Markers   = "MARKERS"sv;
+    static constexpr auto kResName_MeshName  = "MESH NAME"sv;
+    static constexpr auto kResName_Node      = "NODE"sv;
+    static constexpr auto kResName_Nodes     = "NODES"sv;
+    static constexpr auto kResName_Section   = "SECTION"sv;
+    static constexpr auto kResName_Type      = "TYPE"sv;
 }
 
 #endif // LIBIM_RESOURCE_LITERALS_H

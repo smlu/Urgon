4d95da951170dd4b42d5369f55e910ab6fec91f0
smlu <smlu@s5.net>
2019-06-01 02:05:17 +0200

    Add free function 'enumerate'

--- src/libim/utils/utils.h
+++ src/libim/utils/utils.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <sstream>
 #include <string_view>
+#include <tuple>
 #include <type_traits>
 
 namespace libim::utils {
@@ -23,6 +24,31 @@ namespace libim::utils {
             return false;
         }
     }
+
+
+
+    template <typename T,
+              typename TIter = decltype(std::begin(std::declval<T>())),
+              typename = decltype(std::end(std::declval<T>()))>
+    constexpr auto enumerate(T && iterable)
+    {
+        struct iterator
+        {
+            size_t i;
+            TIter iter;
+            bool operator != (const iterator & other) const { return iter != other.iter; }
+            void operator ++ () { ++i; ++iter; }
+            auto operator * () const { return std::tie(i, *iter); }
+        };
+        struct iterable_wrapper
+        {
+            T iterable;
+            auto begin() { return iterator{ 0, std::begin(iterable) }; }
+            auto end() { return iterator{ 0, std::end(iterable) }; }
+        };
+        return iterable_wrapper{ std::forward<T>(iterable) };
+    }
+
 
     template<typename T>
     typename T::iterator copy(typename T::iterator s_begin, std::size_t count, T& dest)

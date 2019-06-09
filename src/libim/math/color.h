b5f96de3ce5a71ea6216166eca4221a9702d3016
smlu <smlu@s5.net>
2019-06-09 17:45:49 +0200

    Refactor: Change struct 'Color' inherit from AbstractColor & Add struct ' ColorRgb'

--- src/libim/math/color.h
+++ src/libim/math/color.h
@@ -5,27 +5,31 @@
 namespace libim {
     struct color_vector_tag {};
 
-    struct Color : public AbstractVector<float, 4, color_vector_tag>
+    template<typename T, std::size_t N>
+    struct AbstractColor : public AbstractVector<T, N, color_vector_tag>
     {
-        using Base_ = AbstractVector<float, 4, color_vector_tag>;
-        using Base_::AbstractVector;
+        //using Base_ = AbstractVector<T, N, color_vector_tag>;
+        //using Base_::AbstractVector;
+       // using AbstractVector<T, N, color_vector_tag>::AbstractVector;
 
-        constexpr inline Color(float r, float g, float b, float a) noexcept :
-            Base_{{{ r, g, b, a }}}
-        {}
+        static_assert (N  == 3 || N == 4);
 
-        explicit constexpr inline Color(std::array<float, 4> a) noexcept :
-            Base_{{ std::move(a) }}
-        {}
+//        constexpr inline Color(float r, float g, float b, float a) noexcept :
+//            Base_{{{ r, g, b, a }}}
+//        {}
 
-        constexpr inline float red() const
+//        explicit constexpr inline AbstractColor(std::array<T, N> a) noexcept :
+//            Base_{{ std::move(a) }}
+//        {}
+
+        constexpr inline T red() const
         {
             return this->at(0);
         }
 
-        constexpr inline void setRed(float r)
+        constexpr inline void setRed(T r)
         {
-            set(0, r);
+            this->set(0, r);
         }
 
         constexpr inline float green() const
@@ -33,31 +37,58 @@ namespace libim {
             return this->at(1);
         }
 
-        constexpr inline void setGreen(float g)
+        constexpr inline void setGreen(T g)
         {
-            set(1, g);
+            this->set(1, g);
         }
 
-        constexpr inline float blue() const
+        constexpr inline T blue() const
         {
             return this->at(2);
         }
 
-        constexpr inline void setBlue(float b)
+        constexpr inline void setBlue(T b)
         {
-            set(2, b);
+            this->set(2, b);
         }
 
-        constexpr inline float alpha() const
+        constexpr inline T alpha() const
         {
             return this->at(3);
         }
 
+        template<bool hasAplha = (N == 4UL), typename = std::enable_if_t<hasAplha>>
         constexpr inline void setAlpha(float a)
         {
-            set(3, a);
+            this->set(3, a);
         }
     };
+
+
+
+
+    struct Color : AbstractColor<float, 4> {};
+    struct ColorRgb : AbstractColor<float, 3> {};
+
+    constexpr inline Color MakeColor(float red, float green, float blue, float alpha)
+    {
+        return Color{{{{ red, green, blue, alpha }}}};
+    }
+
+    constexpr inline Color MakeColor(const ColorRgb& rgb, float alpha)
+    {
+        return Color{{{{ rgb.red(), rgb.green(), rgb.blue(), alpha }}}};
+    }
+
+    constexpr inline ColorRgb MakeColorRgb(float red, float green, float blue)
+    {
+        return ColorRgb{{{{ red, green, blue }}}};
+    }
+
+    constexpr inline ColorRgb MakeColorRgb(const Color& rgba)
+    {
+        return ColorRgb{{{{ rgba.red(), rgba.green(), rgba.blue() }}}};
+    }
 }
 
 #endif // LIBIM_COLOR_H

bf6d2a3131778ee2d6d407e467fa46a907b8973f
smlu <smlu@s5.net>
2020-06-13 15:37:17 +0200

    Update (utils.h): Include missing header

--- libraries/libim/utils/utils.h
+++ libraries/libim/utils/utils.h
@@ -7,12 +7,13 @@
 #include <cstring>
 #include <iomanip>
 #include <optional>
-#include <string>
 #include <sstream>
+#include <string>
 #include <string_view>
 #include <tuple>
 #include <type_traits>
 #include <utility>
+#include <vector>
 
 #define CONCATENATE_DIRECT(s1, s2) s1##s2
 #define CONCATENATE(s1, s2) CONCATENATE_DIRECT(s1, s2)

314e16ce945bb51e399605509ca6edf445e14861
smlu <smlu@s5.net>
2020-06-13 15:30:14 +0200

    Update (ColorFormat):  Add eq. operator ## Add const BGR_888 & ABGR_8888

--- libraries/libim/content/asset/material/colorformat.h
+++ libraries/libim/content/asset/material/colorformat.h
@@ -12,8 +12,8 @@ namespace libim::content::asset {
 
     struct ColorFormat final
     {
-        ColorMode colorMode; // RGB = 1, RGBA = 2
-        uint32_t bpp;        // Bit depth per pixel
+        ColorMode colorMode;
+        uint32_t bpp;        // Color bit depth per pixel
 
         uint32_t redBPP  ;
         uint32_t greenBPP;
@@ -33,13 +33,43 @@ namespace libim::content::asset {
     };
     static_assert(sizeof(ColorFormat) == 56);
 
+    // Predefined color formats
     static constexpr ColorFormat RGB_565   { ColorMode::RGB,   16,   5, 6, 5,   11,  5,  0,   3, 2, 3,   0,  0, 0 };
     static constexpr ColorFormat RGBA_4444 { ColorMode::RGBA,  16,   4, 4, 4,   12,  8,  4,   4, 4, 4,   4,  0, 4 };
     static constexpr ColorFormat ARGB_4444 { ColorMode::RGBA,  16,   4, 4, 4,    8,  4,  0,   4, 4, 4,   4, 12, 4 };
     static constexpr ColorFormat RGBA_5551 { ColorMode::RGBA,  16,   5, 5, 5,   11,  6,  1,   3, 3, 3,   1,  0, 7 };
     static constexpr ColorFormat ARGB_5551 { ColorMode::RGBA,  16,   5, 5, 5,   10,  5,  0,   3, 3, 3,   1, 15, 7 };
     static constexpr ColorFormat RGB_888   { ColorMode::RGB,   24,   8, 8, 8,   16,  8,  0,   0, 0, 0,   0,  0, 0 };
+    static constexpr ColorFormat BGR_888   { ColorMode::RGB,   24,   8, 8, 8,    0,  8, 16,   0, 0, 0,   0,  0, 0 };
     static constexpr ColorFormat RGBA_8888 { ColorMode::RGBA,  32,   8, 8, 8,   24, 16,  8,   0, 0, 0,   8,  0, 0 };
     static constexpr ColorFormat ARGB_8888 { ColorMode::RGBA,  32,   8, 8, 8,   16,  8,  0,   0, 0, 0,   8, 24, 0 };
+    static constexpr ColorFormat ABGR_8888 { ColorMode::RGBA,  32,   8, 8, 8,    0,  8, 16,   0, 0, 0,   8, 24, 0 };
+
+    // Helper functions
+    inline bool operator ==(const ColorFormat& lci, const ColorFormat& rci)
+    {
+        return lci.colorMode == rci.colorMode &&
+               lci.bpp == rci.bpp             &&
+
+               lci.redBPP == rci.redBPP       &&
+               lci.redShl == rci.redShl       &&
+               lci.redShr == rci.redShr       &&
+
+               lci.greenBPP == rci.greenBPP   &&
+               lci.greenShl == rci.greenShl   &&
+               lci.greenShr == rci.greenShr   &&
+
+               lci.blueBPP == rci.blueBPP     &&
+               lci.blueShl == rci.blueShl     &&
+               lci.blueShr == rci.blueShr     &&
+
+               lci.alphaBPP == rci.alphaBPP   &&
+               lci.alphaShl == rci.alphaShl   &&
+               lci.alphaShr == rci.alphaShr;
+    }
+
+    inline bool operator !=(const ColorFormat& lci, const ColorFormat& rci) {
+        return !(lci == rci);
+    }
 }
 #endif // LIBIM_COLORFORMAT_H

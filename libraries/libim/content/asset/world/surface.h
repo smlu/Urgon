a003501a0d28a6a7805d1e50529f1b76a16581c9
smlu <smlu@s5.net>
2020-08-10 22:33:59 +0200

    Update (libim): Update SurfaceFlag

--- libraries/libim/content/asset/world/surface.h
+++ libraries/libim/content/asset/world/surface.h
@@ -11,10 +11,11 @@ namespace libim::content::asset {
     struct Surface final : public Face
     {
         enum SurfaceFlag : uint32_t
-        {                           // 0x0 - can got through the surface
+        {   
+            None                     = 0x0,
             Floor                    = 0x1,
             CogLinked                = 0x2,
-            Impassable               = 0x4,        // Maybe unable for thing(player) to walk on this surface.
+            Passable                 = 0x4,        // Thing can step on this surface
             AiCannotWalkOnFloor      = 0x8,        // Ai won't walk on this surface
             DoubletextureScale       = 0x10,
             HalftextureScale         = 0x20,
@@ -23,23 +24,23 @@ namespace libim::content::asset {
             HorizonSky               = 0x200,
             CeilingSky               = 0x400,
             Scrolling                = 0x800,
-            KillFloor                = 0x1000,     // Player dies if it touches the floor. In cyn_killfloor.cog
+            KillFloor                = 0x1000,     // Player dies if it touches the surface. e.g.: cyn_killfloor.cog
             Climbable                = 0x2000,
             MineCarTrack             = 0x4000,     // Mine car track: sol_ramp.cog, sol_ctrllever5.cog, sol_ctrllever4.cog, sol_ctrllever3.cog, sol_ctrllever2.cog, sol_ctrllever1.cog, sol_comfalls.cog, sol_comentrance.cog
             Metal                    = 0x10000,
             DeepWater                = 0x20000,
             ShallowWater             = 0x40000,
             Earth                    = 0x80000,
-            Web                      = 0x100000,   // Jones engine
+            Web                      = 0x100000,
             Lava                     = 0x200000,
             Snow                     = 0x400000,
             Wood                     = 0x800000,
             Hangable                 = 0x1000000,  // Player can hang on the ledge of this surface. In: tem_bossflood.cog, vol_block_ledgecontrol.cog
             WaterClimbOutLedge       = 0x2000000,
-            UnknownCouldBeWhipSurf   = 0x10000000, // vol_lekk_door.cog
+            WhipAim                  = 0x10000000, // Player starts aming at whippable object on this surface. e.g.: vol_lekk_door.cog, 00_cyn surface no. 1460
             Echo                     = 0x20000000, // indoor surface (eg jumphardecho sound class mode)
             WoodEcho                 = 0x40000000, // indoor wood surface (eg jumpwood sound class mode)
-            EarthEcho                = 0x80000000  // indoor earth surface - vine surface has this flag set (eg jumpearthecho sound class mode)
+            EarthEcho                = 0x80000000  // indoor earth surface - vine surface has this flag set (e.g: jumpearthecho sound class mode)
             //missing flag for stone
         };
 

4c45beff67bdc29fbed4a4ff5a8495683c902d7c
smlu <smlu@s5.net>
2020-12-10 20:35:53 +0100

    Rename & Fix typo light mode enumerators

--- libraries/libim/content/asset/primitives/light_mode.h
+++ libraries/libim/content/asset/primitives/light_mode.h
@@ -5,12 +5,12 @@
 namespace libim::content::asset {
     enum class LightMode : uint32_t
     {
-        FullyLit = 0,
-        NotLit   = 1,
-        Fiffuse  = 2,
-        Gouraud  = 3,
-        Gouraud2 = 4,
-        Gouraud3 = 5
+        Unlit     = 0,
+        Lit       = 1,
+        Diffuse   = 2,
+        Gouraud   = 3,
+        Gouraud2  = 4,
+        Gouraud3  = 5
     };
 }
 #endif // LIBIM_LIGHT_MODE_H

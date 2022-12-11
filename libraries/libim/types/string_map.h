c1899ccc53b29d25c790777a639531b754343cb1
Crt Vavros - smlu <smlu@s5.net>
2022-12-11 23:19:58 +0100

    [libim] Refactor StringMap to support different comparator
    Added comparator StringCaseInsensitiveEqual.
    Renamed comparator CaseInsensitiveComparator  -> StringCaseInsensitiveLess

--- libraries/libim/types/string_map.h
+++ libraries/libim/types/string_map.h
@@ -7,7 +7,7 @@
 #include <type_traits>
 
 namespace libim{
-    struct CaseInsensitiveComparator
+    struct StringCaseInsensitiveLess
     {
         bool operator()(const std::string_view lhs, const std::string_view rhs) const
         {
@@ -18,14 +18,27 @@ namespace libim{
         }
     };
 
+    struct StringCaseInsensitiveEqual
+    {
+        bool operator()(const std::string_view lhs, const std::string_view rhs) const
+        {
+            return lhs.size() == rhs.size() &&
+                std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(),
+                    [](const char& lhs, const char& rhs) {
+                        return std::tolower(lhs) == std::tolower(rhs);
+                });
+        }
+    };
+
     /**
-     * A std::map alias that uses a case-insensitive string comparator for key.
+     * A std::map alias for string which uses a case-insensitive string for key.
      *
      * @tparam StrT   - The key string type. Must be convertible to std::string_view.
      * @tparam ValueT - The value type.
+     * @tparam PredT  - The predicate type. By default, CaseInsensitiveLess is used.
      */
-    template<typename StrT, typename ValueT,
+    template<typename StrT, typename ValueT, typename PredT = StringCaseInsensitiveLess,
         typename = std::enable_if_t<std::is_convertible_v<StrT, std::string_view>>>
-    using StringMap = std::map<StrT, ValueT, CaseInsensitiveComparator>;
+    using StringMap = std::map<StrT, ValueT, PredT>;
 }
 #endif // LIBIM_STRING_MAP_H

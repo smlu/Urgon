4d4f4cd69881b708eb9ef4c131213baf3f4dee3f
Crt Vavros - smlu <smlu@s5.net>
2022-12-17 19:01:48 +0100

    [libim] Refactor `FixedString`
    Added template bool param `nullTermination` which determines if null char should be set at the end of array after copying the string. 
    Added constexpr `size()` function.
    Added constexpr specifier to `operator std::string_view`.
    Deleted `empty()` memeber function.

--- libraries/libim/types/fixed_string.h
+++ libraries/libim/types/fixed_string.h
@@ -7,21 +7,34 @@
 #include <string_view>
 #include <stdexcept>
 
+#include <libim/math/math.h>
 #include <libim/utils/utils.h>
 
 namespace libim {
 
-    /** FixedString represents fixed size string aka char[Size] */
-    template<std::size_t N>
+    /**
+     * FixedString represents fixed size string aka char[Size].
+     * It is a wrapper around std::array<char, Size>.
+     *
+     * @tparam N Size of the string
+     * @tparam nullTermination If true, the last element of the string will be set to 0. Default: true.
+     */
+    template<std::size_t N, bool nullTermination = true>
     struct FixedString : std::array<char, N>
     {
+        static_assert(nullTermination ? N >= 2 : N >= 1, "FixedString size must be at least 1, or 2 if nullTermination is true");
+
         using base_type = std::array<char, N>;
         using std::array<char, N>::array;
+
+        constexpr bool empty() const noexcept = delete;
 
         constexpr FixedString() : base_type{ 0 } {}
         constexpr FixedString(base_type a) : base_type(std::move(a))
         {
-            this->operator[](N-1) = 0;
+            if constexpr(nullTermination) {
+                this->operator[](libim::min(N - 1, this->size())) = 0;
+            }
         }
 
         constexpr FixedString(std::string_view str) : base_type{ 0 }
@@ -30,7 +43,9 @@ namespace libim {
                 throw std::invalid_argument("Invalid str size to init FixedString");
             }
             std::copy(str.begin(), str.end(), this->begin());
-            this->operator[](N-1) = 0;
+            if constexpr(nullTermination)  {
+                this->operator[](libim::min(N - 1, this->size())) = 0;
+            }
 
         }
 
@@ -38,8 +53,10 @@ namespace libim {
         constexpr FixedString(const char (&str)[C]) : base_type{ 0 }
         {
             static_assert(C <= N, "Invalid string literal size to init FixedString");
-            std::copy(str, str + C, this->begin()); // Note: since C++20 constexpr
-            this->operator[](N-1) = 0;
+            std::copy(str, str + C, this->begin()); // Note: constexpr since C++20
+            if constexpr(nullTermination) {
+                this->operator[](libim::min(N - 1, this->size())) = 0;
+            }
         }
 
         std::string toStdString() const
@@ -52,35 +69,46 @@ namespace libim {
             return toStdString();
         }
 
-        operator std::string_view() const // Warning: It is dangerous to implicitly return string_view!
+        constexpr operator std::string_view() const // Warning: It is dangerous to implicitly return string_view!
         {
-            auto endIt = std::find(this->begin(), this->end(), '\0');
-            return std::string_view(this->data(), std::distance(this->begin(), endIt));
+            return std::string_view(this->data(), this->size());
         }
 
-        bool operator == (const FixedString& rstr) const
+        constexpr bool operator == (const FixedString& rstr) const
         {
             for (std::size_t i = 0; i < N; i++)
             {
-                if(this->at(i) != rstr.at(i)) {
+                if (this->at(i) != rstr.at(i)) {
                     return false;
                 }
-                else if(this->at(i) == 0) {
+                else if (this->at(i) == 0) {
                     return true;
                 }
             }
             return true;
         }
 
-        constexpr bool isEmpty() const {
-            return N == 0 || this->at(0) == 0;
+        constexpr typename base_type::size_type size() const noexcept
+        {
+            /* Note the size can't be cached because it changes the interface struct which is then not plain char[] anymore */
+            std::size_t size = 0;
+            const char* pNullCh = std::char_traits<char>::find(this->data(), N, '\0');
+            if (pNullCh) {
+                size = pNullCh - this->data();
+            };
+            return size;
+        }
+
+        constexpr bool isEmpty() const
+        {
+            return N == 0 || this->at(0) == 0; // N == no use due to static check
         }
     };
 
-    template<std::size_t N>
-    std::ostream& operator<<(std::ostream& os, const FixedString<N>& cstr)
+    template<std::size_t N, bool nullTermination>
+    std::ostream& operator<<(std::ostream& os, const FixedString<N, nullTermination>& cstr)
     {
-        os << cstr.data();
+        os << std::string_view(cstr.data(), cstr.size());
         return os;
     }
 }

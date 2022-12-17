ac8a9b0e17298b1755bbb0c59d28f669a144a9a4
Crt Vavros - smlu <smlu@s5.net>
2022-12-17 18:54:39 +0100

    [libim] Add COG new flag `ThingLinked`

--- libraries/libim/content/asset/cog/cogscript.h
+++ libraries/libim/content/asset/cog/cogscript.h
@@ -16,17 +16,17 @@ namespace libim::content::asset {
 
         enum Flag
         {
-            None      = 0x00,
-            Debug     = 0x01,
-            Disabled  = 0x02,
-            PulseSet  = 0x04,
-            TimerSet  = 0x08,
-            Paused    = 0x10,
-            Unknown20 = 0x20,    // Set when thing/template assigns cog
-            Local     = 0x40,    // Cog runs on the client and server
-            Server    = 0x80,    // Cog runs only on the server, client messages are forwarded to the server
-            Global    = 0x100,   // Cog runs locally on all machines
-            NoSync    = 0x200,   // Cog results are not broadcast to the other machines.
+            None        = 0x00,
+            Debug       = 0x01,
+            Disabled    = 0x02,
+            PulseSet    = 0x04,
+            TimerSet    = 0x08,
+            Paused      = 0x10,
+            ThingLinked = 0x20,    // Cog is linked to a thing, i.e it is a thing cog script
+            Local       = 0x40,    // Cog runs on the client and server
+            Server      = 0x80,    // Cog runs only on the server, client messages are forwarded to the server
+            Global      = 0x100,   // Cog runs locally on all machines
+            NoSync      = 0x200,   // Cog results are not broadcast to the other machines.
         };
 
         Flags<Flag> flags;
